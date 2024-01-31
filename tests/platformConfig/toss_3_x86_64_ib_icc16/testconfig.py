5b5db5f272b69ddbe0e4fa052e5038848533e1c2
Christopher Sherman <sherman27@llnl.gov>
2024-01-31 11:08:30 -0800

    Removing old baselines

--- tests/platformConfig/toss_3_x86_64_ib_icc16/testconfig.py
+++
@@ -1,6 +0,0 @@
-# Source parent platform configuration file, but only if this is the first configuration file read.
-# If we got here otherwise, we assume that the parent config file sent us here.
-if configDepth == 1:
-    sourceParentConfig()
-# Setup executable path with default path.  This is a required call and should be called before the baseline directory is set.
-# machine.setGeosxPath("/g/g14/corbett5/geosx/build-toss_3_x86_64_ib-gcc@7.1.0-debug/bin")
